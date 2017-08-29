64a35d2e1e6759c915575cf461a70c0446fa9dbe
Amol Jadhav <amolj@winsoftech.com>
2017-08-29 16:08:03 +0530

    added new sample class for checking sonar violations

---
+++ src/main/java/com/example/SampleModel.java
@@ -0,0 +1,23 @@
+package com.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class SampleModel {
+    private static final Logger logger = LoggerFactory.getLogger(SampleModel.class);
+    
+    public void test(boolean isSuccess) {
+        logger.debug("test");
+
+        if (isSuccess) {
+            if (isSuccess) {
+                if (isSuccess) {
+                    if (isSuccess) {
+                        System.out.println("This is sample class to check sonar violations");
+                    }
+                }
+            }
+        }
+    }
+
+}
