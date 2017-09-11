c647112ab7b5c40754b943d2cf1ba69e69084e78
Mohan Medarametla <mohanm@winsoftech.com>
2017-09-11 19:27:16 +0530

    added violations for sonar report

--- src/main/java/com/example/SampleModel.java
+++ src/main/java/com/example/SampleModel.java
@@ -47,4 +47,18 @@ public void test2(boolean isSuccess) {
             }
         }
     }
+
+public void test3(boolean isSuccess) {
+        logger.debug("test");
+
+        if (isSuccess) {
+            if (isSuccess) {
+                if (isSuccess) {
+                    if (isSuccess) {
+                        System.out.println("This is sample class to check sonar violations");
+                    }
+                }
+            }
+        }
+    }
 }
